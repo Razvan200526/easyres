289bee281d469629523215db7cd72eca6c67bb98
RazvanAndrei200526 <doruippu@gmail.com>
2025-12-05 18:30:40 +0200

    migrated primarymailer from smtp using resend to Resend class

--- server/src/shared/mail/PrimaryMailer.ts
+++ server/src/shared/mail/PrimaryMailer.ts
@@ -1,37 +1,30 @@
 import { env } from '@shared/env';
 import { color } from 'console-log-colors';
 import { createTransport } from 'nodemailer';
+import { Resend } from 'resend';
 import type { Mailer, SendMailParams } from './types';
 export class PrimaryMailer implements Mailer {
   private apiKey: string;
-
+  private resend: Resend;
   constructor() {
     console.debug(color.bgRedBright('Creating primary mailer'));
 
     //@ts-ignore
     this.apiKey = env.RESEND_API_KEY || '';
-  }
-
-  public async send({ to, subject, html }: SendMailParams): Promise<void> {
     if (!this.apiKey) {
       console.error('RESEND_API_KEY is not set - cannot send email');
       throw new Error('Mail configuration error: RESEND_API_KEY is not set');
     }
+    this.resend = new Resend(this.apiKey);
+  }
 
-    const mailer = createTransport({
-      host: 'smtp.resend.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'resend',
-        pass: this.apiKey,
-      },
-    });
-    await mailer.sendMail({
+  public async send({ to, subject, html }: SendMailParams): Promise<void> {
+
+    await this.resend.emails.send({
       from: '"EasyRes" <welcome@resumetracker.me>',
       to,
       subject,
-      html,
+      react: html,
     });
   }
 }
