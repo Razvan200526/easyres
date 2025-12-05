c69db9d6d5158e65199918cc7910983411599623
RazvanAndrei200526 <doruippu@gmail.com>
2025-12-05 23:50:42 +0200

    fix mail template

--- server/src/auth/mailers/forgotPassword/ForgetPasswordEmailCheckMailer.ts
+++ server/src/auth/mailers/forgotPassword/ForgetPasswordEmailCheckMailer.ts
@@ -1,7 +1,7 @@
 import { getMailer } from '@server/shared/mail/getMailer';
 import type { Mailer } from '@server/shared/mail/types';
-import { renderTemplate } from '../renderers';
-import { ForgetPasswordEmailCheckTemplate } from './ForgetPasswordEmailCheckTemplate';
+// import { renderTemplate } from '../renderers';
+// import { ForgetPasswordEmailCheckTemplate } from './ForgetPasswordEmailCheckTemplate';
 
 export class ForgetPasswordEmailCheckMailer {
   private readonly mailer: Mailer;
@@ -9,14 +9,15 @@ export class ForgetPasswordEmailCheckMailer {
     this.mailer = getMailer();
   }
 
-  public async send(config: { to: string; otp: string; lang?: string }) {
-    const html = await renderTemplate(
-      ForgetPasswordEmailCheckTemplate({ otp: config.otp }),
-    );
-    await this.mailer.send({
-      to: [config.to],
-      subject: 'Reset your password',
-      html,
-    });
+  public async send(_config: { to: string; otp: string; lang?: string }) {
+    // const html = await renderTemplate(
+    //   ForgetPasswordEmailCheckTemplate({ otp: config.otp }),
+    // );
+    // await this.mailer.send({
+    //   to: [config.to],
+    //   subject: 'Reset your password',
+    //   html,
+    // });
+    console.debug(this.mailer);
   }
 }
